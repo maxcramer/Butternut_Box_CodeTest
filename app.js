894d57854afdaf35e10648b3121aa2ded390d811
Max Cramer <maximiliancramer@yahoo.com>
2019-10-24 00:32:40 +0100

    added functionality, only able to select one option

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-var tickBackgroundOne = document.getElementById('inner__circle__tick__one');
 
-$(tickBackgroundOne).click(function(){
+var tickBackgroundOne = document.getElementById('inner__circle__tick__one');
+var tickBackgroundTwo = document.getElementById('inner__circle__tick__two');
+
+
+$(tickBackgroundOne).click('click', function(){
   $(tickBackgroundOne).css('background-color', '#1E5D8A');
+  $(tickBackgroundTwo).css('background-color', 'white');
 });
 
-var tickBackgroundTwo = document.getElementById('inner__circle__tick__two');
 
 $(tickBackgroundTwo).click(function(){
   $(tickBackgroundTwo).css('background-color', '#1E5D8A');
+  $(tickBackgroundOne).css('background-color', 'white');
 });
