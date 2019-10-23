77490fe61c29f2202ab071129989b540c4aff132
Max Cramer <maximiliancramer@yahoo.com>
2019-10-24 00:20:04 +0100

    background changing color on click, written in jquery

---
+++ app.js
@@ -0,0 +1,11 @@
+var tickBackgroundOne = document.getElementById('inner__circle__tick__one');
+
+$(tickBackgroundOne).click(function(){
+  $(tickBackgroundOne).css('background-color', '#1E5D8A');
+});
+
+var tickBackgroundTwo = document.getElementById('inner__circle__tick__two');
+
+$(tickBackgroundTwo).click(function(){
+  $(tickBackgroundTwo).css('background-color', '#1E5D8A');
+});
