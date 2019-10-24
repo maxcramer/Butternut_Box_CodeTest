1a99cae1e633cb649f010ecb294eacf437eccc5f
Max Cramer <maximiliancramer@yahoo.com>
2019-10-24 01:11:56 +0100

    Testing how to create stars on selected, waiting for assets

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 var tickBackgroundOne = document.getElementById('inner__circle__tick__one');
 var tickBackgroundTwo = document.getElementById('inner__circle__tick__two');
 
-
 $(tickBackgroundOne).click('click', function(){
   $(tickBackgroundOne).css('background-color', '#1E5D8A');
   $(tickBackgroundTwo).css('background-color', 'white');
